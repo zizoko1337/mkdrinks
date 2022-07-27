de979d540e1324cb6a9c04dffe824c7632b9071f
zizoko1337 <95056942+zizoko1337@users.noreply.github.com>
2022-07-28 00:56:52 +0200

    added routes

---
+++ src/components/pages/LogIn.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>Log In</h1>
+    <h2>login</h2>
+    <h2>password</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LogIn',
+
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+    color: white;
+}
+h2{
+    color: white;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.hello {
+  background: #050131;
+  background-repeat: no-repeat, repeat;
+  background-size: 100%;
+  min-height: 1000px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router';
-import App from './App.vue'
+import App from './App.vue';
+import HomePage from './components/pages/HomePage.vue';
+import LogIn from './components/pages/LogIn.vue';
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-
+        { path: '/home', component: HomePage },
+        { path: '/login', component: LogIn },
     ]
 });
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app')
